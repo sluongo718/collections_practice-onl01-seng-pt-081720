b1ec320cafa558ed0473ea3e0c7da259083eeab0
sam l <sluongo718@yahoo.com>
2020-08-25 02:46:48 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Bonus — Collections Practice
+m# Bonus — Collections Practice
 
 ## Objectives
 
--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,63 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+  
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(a)
+  a[0], a[1], a[2] = a[0], a[2], a[1]
+  
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+   array.each do |name|
+    name[2] = "$"
+    
+    
+  end
+
+end
+
+def find_a(array)
+  array.select do |word|
+    word.start_with?("a")
+  
+end
+  
+end
+
+def sum_array(array)
+  array.inject(:+)
+
+end
+
+def add_s(array)
+  
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+    
+  end 
+
+end 
+
+
+
+
 
